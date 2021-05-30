a5b6c64395cd9645bad393cfa6d3224c1e703093
Joshua Burt <joshuaadburt@gmail.com>
2021-05-30 17:14:38 -0300

    Init Commit

---
+++ Assets/Scripts/Planet.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Planet : MonoBehaviour {
+    public float radius = 2;
+    // Start is called before the first frame update
+    void Start() {
+        GameObject cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
+
+        cube.transform.localScale = new Vector3(radius,radius,radius);
+    }
+
+    // Update is called once per frame
+    void Update() {
+        
+    }
+}
---
+++ Assets/Scripts/TPMPlayerMove.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using UnityEngine;
+
+/**
+ * Script from:
+ * https://forum.unity.com/threads/third-person-camera-movement-script.858673/
+ *
+ * By Alsda_Gamer
+ * Edited By Joshua Burt
+ */
+
+public class TPMPlayerMove : MonoBehaviour {
+	public float playerSpeed = 2.0f;
+	public float jumpHeight = 1.0f;
+	public float gravityValue = 9.81f;
+	
+	private CharacterController controller;
+	private Vector3 playerVelocity;
+	private Camera _cam;
+
+	void Start() {
+		_cam = Camera.main;
+		controller = GetComponent<CharacterController>();
+	}
+
+	void Update() {
+		float Horizontal = Input.GetAxis("Horizontal") * playerSpeed * Time.deltaTime;
+		float Vertical = Input.GetAxis("Vertical") * playerSpeed * Time.deltaTime;
+		
+		var camTransform = _cam.transform;
+		
+		// Finds the new placement of the player based on the inputted buttons
+		Vector3 Movement = camTransform.right * Horizontal + camTransform.forward * Vertical;
+		
+		// Controls the falling of the player when they are not on the ground
+		
+		if(controller.isGrounded) {
+			Movement.y = 0f;
+			
+			if(Input.GetAxis("Jump") != 0) {
+				Movement.y = jumpHeight * Time.deltaTime;
+			}
+		} else {
+			Movement.y -= Mathf.Sqrt(gravityValue * Time.deltaTime);
+		}
+
+		controller.Move(Movement);
+
+		if(Movement.x != 0f || Movement.z != 0f) {
+			Quaternion lookRotation = Quaternion.LookRotation(Movement);
+			lookRotation.x = 0f;
+			lookRotation.z = 0f;
+				
+			transform.rotation = Quaternion.Lerp(transform.rotation, lookRotation, 0.1f);
+		}
+	}
+}
