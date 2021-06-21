5e4fc6e62ce805eda3b0f1b69a8b39e896f8e8fc
Joshua Burt <joshuaadburt@gmail.com>
2021-06-20 22:18:15 -0300

    Planets now have a reticle and distance meter

--- Assets/Scripts/PlanetHoverOverlay.cs
+++ Assets/Scripts/PlanetHoverOverlay.cs
@@ -10,74 +10,81 @@ public class PlanetHoverOverlay : MonoBehaviour {
     public GameObject image;
     private GameObject clone;
     private GameObject canvas;
+    private Transform _reticle;
+    private Transform _distance;
+    private Text _distanceText;
+    private Image _image;
+    private RectTransform _imgTransform, _textTransform;
 
     void Start() {
         _cam = Camera.main;
         _transform = transform;
         canvas = GameObject.Find("Canvas");
+        
         clone = Instantiate(image, canvas.transform, true);
-        clone.GetComponent<Image>().color = GetComponent<ColourSettings>().planetColour;
+        clone.transform.SetSiblingIndex(0);
+
+        _distance = clone.transform.GetChild(0);
+        _reticle = clone.transform.GetChild(1);
+
+        _image = _reticle.GetComponent<Image>();
+        _imgTransform = _reticle.GetComponent<RectTransform>();
+        _image.color = GetComponent<ColourSettings>().planetColour;
+        
+        _distanceText = _distance.GetComponent<Text>();
+        _textTransform = _distance.GetComponent<RectTransform>();
+        _distanceText.color = GetComponent<ColourSettings>().planetColour;
     }
 
     private void Update() {
         if(_cam is { } && clone) {
             Vector3 pos = _cam.WorldToScreenPoint(_transform.position);
 
-            if(pos.x > 0 && pos.x < Screen.width && pos.y > 0 && pos.y < Screen.height) {
-                if((_cam.transform.position - _transform.position).magnitude >
-                   GetComponent<ShapeSettings>().planetRadius * 9) {
-                    clone.SetActive(true);
+            if(pos.x > 0 && pos.x < Screen.width && pos.y > 0 && pos.y < Screen.height && 
+               (_cam.transform.position - _transform.position).magnitude > GetComponent<ShapeSettings>().planetRadius * 9) {
+                clone.SetActive(true);
+                
+                float fadingValue = Math.Abs(1000 / distance(pos.x, pos.y, Screen.width / 2f, Screen.height / 2f));
 
-                    Image img = clone.GetComponent<Image>();
+                var imgColour = _image.color;
+                imgColour.a = fadingValue;
+                _image.color = imgColour;
+                
+                var textColour = _distanceText.color;
+                textColour.a = fadingValue;
+                _distanceText.color = textColour;
 
-                    var tempColor = img.color;
-                    tempColor.a = Math.Abs(1000 / distance(pos.x, pos.y, Screen.width / 2, Screen.height / 2));
-                    img.color = tempColor;
+                float apparentSize = DistanceAndDiameterToPixelSize();
+                var imgLocalScale = _imgTransform.localScale;
+                
+                float imgSize =  Mathf.Clamp(apparentSize, 0.5f, 4f);
+                imgLocalScale.x = imgSize / 2;
+                imgLocalScale.y = imgSize / 2;
 
-                    RectTransform rectTransform = clone.GetComponent<RectTransform>();
-                    rectTransform.position = new Vector2(pos.x, pos.y);
+                _imgTransform.localScale = imgLocalScale;
 
-                    var rectTransformLocalScale = rectTransform.localScale;
-                    rectTransformLocalScale.x = DistanceAndDiameterToPixelSize() / 100;
-                    rectTransformLocalScale.y = DistanceAndDiameterToPixelSize() / 100;
-
-                    rectTransform.localScale = rectTransformLocalScale;
-                }
+                float textSize = 0.5f;
+                var textLocalScale = _textTransform.localScale;
+                textLocalScale.x = textSize; 
+                textLocalScale.y = textSize; 
+                _textTransform.localScale = textLocalScale;
+                
+                _imgTransform.position = new Vector2(pos.x, pos.y);
+                _textTransform.position = new Vector2(_imgTransform.position.x + _imgTransform.rect.width + 30, pos.y);
             } else {
                 clone.SetActive(false);
             }
+            
+            _distanceText.text = "Distance: " + Math.Round((_transform.position - _cam.transform.position).magnitude) + "km"; 
         }
     }
     
     //Get the screen size of an object in pixels, given its distance and diameter.
     float DistanceAndDiameterToPixelSize(){
-        float pixelSize = GetComponent<ShapeSettings>().planetRadius * Mathf.Rad2Deg * Screen.height 
-                          / ((_transform.position - _cam.transform.position).magnitude * _cam.fieldOfView);
+        var pixelSize = GetComponent<ShapeSettings>().planetRadius * Mathf.Rad2Deg * Screen.height 
+                        / ((_transform.position - _cam.transform.position).magnitude * _cam.fieldOfView);
         return pixelSize;
     }
-    
-    private static Rect GUIRectWithObject(GameObject go) {
-        Vector3 cen = go.GetComponent<Renderer>().bounds.center;
-        Vector3 ext = go.GetComponent<Renderer>().bounds.extents;
-        Vector2[] extentPoints = new Vector2[8] {
-            HandleUtility.WorldToGUIPoint(new Vector3(cen.x-ext.x, cen.y-ext.y, cen.z-ext.z)),
-            HandleUtility.WorldToGUIPoint(new Vector3(cen.x+ext.x, cen.y-ext.y, cen.z-ext.z)),
-            HandleUtility.WorldToGUIPoint(new Vector3(cen.x-ext.x, cen.y-ext.y, cen.z+ext.z)),
-            HandleUtility.WorldToGUIPoint(new Vector3(cen.x+ext.x, cen.y-ext.y, cen.z+ext.z)),
-            HandleUtility.WorldToGUIPoint(new Vector3(cen.x-ext.x, cen.y+ext.y, cen.z-ext.z)),
-            HandleUtility.WorldToGUIPoint(new Vector3(cen.x+ext.x, cen.y+ext.y, cen.z-ext.z)),
-            HandleUtility.WorldToGUIPoint(new Vector3(cen.x-ext.x, cen.y+ext.y, cen.z+ext.z)),
-            HandleUtility.WorldToGUIPoint(new Vector3(cen.x+ext.x, cen.y+ext.y, cen.z+ext.z))
-        };
-        Vector2 min = extentPoints[0];
-        Vector2 max = extentPoints[0];
-        foreach(Vector2 v in extentPoints) {
-            min = Vector2.Min(min, v);
-            max = Vector2.Max(max, v);
-        }
-        return new Rect(min.x, min.y, max.x-min.x, max.y-min.y);
-    }
-
 
     private float distance(float x1, float y1, float x2, float y2) {
         return (x1 - x2) * (x1 - x2) + (y1 - y2) * (y1 - y2);
